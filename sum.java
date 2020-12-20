40fc0c79bf840aae312cf7df4abd09063755ca7d
Vedant1932 <vedantsagde2000@gmail.com>
2020-12-20 16:17:17 +0530

    Addition

---
+++ sum.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Sum
+{
+        public static void main(String[] args)
+        {
+            int a=10,b=20;
+            int sum=a+b;
+            System.out.println("Sum ="+sum);
+        }
+}
